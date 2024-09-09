768cb515acd9d0e4c5990a6236e8143431548d0d
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-09 03:00:52 +0000

    add: user, passport & jwt integration

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { PrismaService } from "src/prisma.service";
+import { JwtStrategy } from "./jwt.strategy";
+import { PassportModule, PassportStrategy } from "@nestjs/passport";
+import { UsersService } from "src/users/users.service";
+import { UsersModule } from "src/users/users.module";
+import { JwtModule } from "@nestjs/jwt";
+
+@Module({
+    controllers: [AuthController],
+    providers: [AuthService,PrismaService,JwtStrategy,UsersService],
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: {
+                expiresIn: process.env.EXPIRES_IN
+
+            }
+        })
+
+    ]
+
+})
+export class AuthModule {
+
+}
