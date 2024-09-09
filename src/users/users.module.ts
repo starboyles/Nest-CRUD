6b0795c53e48536352f0fce4ca1b3bbea465ea95
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-09 02:58:13 +0000

    fix: usersModule typo

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { PrismaService } from "src/prisma.service";
 
 @Module({
-    controllers: [],
-    providers: [],
+    controllers: [UsersController],
+    providers: [UsersService, PrismaService],
 })
 
-export class usersModule {}
+export class UsersModule {}
