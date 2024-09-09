44881f7214343a5e9e69c871bdd2aa56a3dd74a9
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-09 03:01:40 +0000

    Refactor to import Users & AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module'; // Import the UsersModule from the correct file
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
