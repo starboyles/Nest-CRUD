2619acfee4026fcf257841aa314a4d973c8219c7
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-09-09 05:24:26 +0000

    fix: typo + code format

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module'; // Import the UsersModule from the correct file
+import { UsersModule } from './users/users.module'; 
 import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
